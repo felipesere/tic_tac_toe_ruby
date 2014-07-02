549718e5bbfe8adf9e5cca3b5a3afe1117049028
Felipe Sere <felipesere@gmail.com>
2014-07-02 11:56:41 +0100

    remove duplication by extracting 'let'

--- spec/tic_tac_toe/ui/cli_interface_spec.rb
+++ spec/tic_tac_toe/ui/cli_interface_spec.rb
@@ -2,20 +2,21 @@ require 'spec_helper'
 
 describe CliInterface do
   let(:board) { Board.create_empty }
+  let(:fake_out) { StringIO.new }
+  let(:display) { CliInterface.new(nil, fake_out) }
+  
+  before(:each) do
+    fake_out.rewind
+  end
+
   context "#render" do
     it "returns a blank ASCII board when the board is empty" do
-      fake_out = StringIO.new
-      display = CliInterface.new(StringIO.new, fake_out)
-
       display.render(board)
       expect(fake_out.string).to eq("[1][2][3]\n[4][5][6]\n[7][8][9]\n")
     end
 
     it "shows the player name when marked" do
       marked_board = board.perform_move(:x, board.possible_moves.first)
-      fake_out = StringIO.new
-      display = CliInterface.new(StringIO.new, fake_out)
-
       display.render(marked_board)
       expect(fake_out.string).to eq("[x][2][3]\n[4][5][6]\n[7][8][9]\n")
     end
@@ -35,13 +36,6 @@ describe CliInterface do
   end
 
   context "messages" do
-    let(:fake_out) { StringIO.new }
-    let(:display) { CliInterface.new(nil, fake_out) }
-
-    before(:each) do
-      fake_out.rewind
-    end
-
     it "displays a proper error message" do
       display.input_error("foo")
       expect(fake_out.string).to include('foo')
