43b8ab664c396f91ed3370f76dbb2835fb376fce
Felipe Sere <felipesere@gmail.com>
2014-07-23 15:50:54 +0100

    preview the board in the shoes binary

--- bin/shoesttt.rb
+++ bin/shoesttt.rb
@@ -1,14 +1,15 @@
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
 require 'gui/main_pane'
-require 'gui/cell'
+require 'gui/board'
 require 'tic_tac_toe/players/player_factory'
+require 'tic_tac_toe/board'
+require 'spec/gui/fakes'
 
 Shoes.app do
   factory = TicTacToe::Players::PlayerFactory.new({io: false})
   GUI::MainPane.new(app, factory.player_combinations ).draw do |players|
     puts players
   end
-  [:x, :o, nil].each do |mark|
-    GUI::Cell.new(app, mark, nil)
-  end 
+  board = TicTacToe::Board.new([[:x, :o, :x],[:x, :o, :x],[:x, :o, :x]])
+  GUI::Board.new(app,board, GUI::FakeClicker.new) 
 end
